8ba6f632d78ed557d36abc89875fcc5b101cfd00
Dominik <zrebcu@gmail.com>
2017-03-14 16:54:58 +0100

    changes header breakpoints, adds icon fonts to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
-const toggleIcon = document.querySelector('.toggle-icon');
+const toggleIcon = document.querySelector('.button-toggle');
 const nav = document.querySelector('.nav');
+const navSearchInput = document.querySelector('.nav__input');
+const buttonSearch = document.querySelector('.button-search');
 
 function toggleOpen() {
   nav.classList.toggle('nav--open');
 }
 
+function searchOpen() {
+  navSearchInput.classList.toggle('nav__input--visible');
+}
+
 toggleIcon.addEventListener('click', toggleOpen);
+buttonSearch.addEventListener('click', searchOpen);
+

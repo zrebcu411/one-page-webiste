66fb8516dea060325c8585e4a7b5ba3c81bac76f
Dominik <zrebcu@gmail.com>
2017-04-11 19:55:21 +0200

    adds completed, responsive slider

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,35 @@ function removeFill() {
 
 svgIcons.forEach(svg => svg.addEventListener('mouseleave', removeFill));
 svgIcons.forEach(svg => svg.addEventListener('mouseover', fill));
+
+//slider
+const slides = document.querySelectorAll('.slider__slide');
+const nextSlideButton = document.querySelector('.slider__button-right');
+const prevSlideButton = document.querySelector('.slider__button-left');
+
+let current = 0;
+
+function goToSlide(n) {
+  slides[current].classList.remove('slider__slide--active');
+  current = (n + slides.length)%slides.length;
+  slides[current].classList.add('slider__slide--active');
+}
+
+function playSlide() {
+  goToSlide(current + 1);
+}
+
+function nextSlide() {
+  clearInterval(slideTimer);
+  goToSlide(current + 1);
+}
+
+function prevSlide() {
+  clearInterval(slideTimer);
+  goToSlide(current - 1);
+}
+
+let slideTimer = setInterval(playSlide, 3500);
+nextSlideButton.addEventListener('click', nextSlide);
+prevSlideButton.addEventListener('click', prevSlide);
+

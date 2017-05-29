268ecaafbd6334d33980e7e1aef6fd8cb3ac9d0a
Dominik <zrebcu@gmail.com>
2017-05-29 22:55:24 +0200

    adds some animations when scrolling

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
-gulp.task('styles', function() {
-    gulp.src('./stylesheets/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./stylesheets/'));
+gulp.task('minify-css', function() {
+    return gulp.src('stylesheets/style.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('stylesheets'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('start', function() {
-    gulp.watch('./stylesheets/**/*.scss', ['styles']);
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: ''
+        }
+    });
+});
+
+gulp.task('sass', function() {
+    gulp.src('stylesheets/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('stylesheets'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('start', ['browser-sync', 'sass', 'minify-css'], function() {
+    gulp.watch('stylesheets/**/*.scss', ['sass']);
+    gulp.watch('stylesheets/*.css', ['minify-css']);
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/*.js', browserSync.reload);
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,127 +1,141 @@
-const toggleIcon = document.querySelector('.button-toggle');
-const buttonSearch = document.querySelector('.button-search');
+(function(namespace) {
 
-function toggleOpen() {
-  const nav = document.querySelector('.nav');
-  nav.classList.toggle('nav--open');
-  toggleIcon.setAttribute('aria-expanded', nav.classList.contains('nav--open'));
-}
+  // Opening / closing navigation on mobile
+  const toggleIcon = document.querySelector('.button-toggle');
+  const buttonSearch = document.querySelector('.button-search');
 
-function searchOpen() {
-  const navSearchInput = document.querySelector('.nav__input');
-  navSearchInput.classList.toggle('nav__input--visible');
-  buttonSearch.setAttribute('aria-expanded', navSearchInput.classList.contains('nav__input--visible'));
-}
+  function toggleOpen() {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('nav--open');
+    toggleIcon.setAttribute('aria-expanded', nav.classList.contains('nav--open'));
+  }
 
-toggleIcon.addEventListener('click', toggleOpen);
-buttonSearch.addEventListener('click', searchOpen);
+  function searchOpen() {
+    const navSearchInput = document.querySelector('.nav__input');
+    navSearchInput.classList.toggle('nav__input--visible');
+    buttonSearch.setAttribute('aria-expanded', navSearchInput.classList.contains('nav__input--visible'));
+  }
 
-const svgIcons = document.querySelectorAll('.socials__svg');
-const svgPaths = document.querySelectorAll('.socials__path');
+  toggleIcon.addEventListener('click', toggleOpen);
+  buttonSearch.addEventListener('click', searchOpen);
 
-function fill() {
-  const child = this.querySelector('.socials__path');
-  child.classList.add('socials__path--filled');
-}
 
-function removeFill() {
-  const child = this.querySelector('.socials__path');
-  child.classList.remove('socials__path--filled');
-}
+  // SVG icons filling on hover
+  const svgIcons = document.querySelectorAll('.socials__svg');
+  const svgPaths = document.querySelectorAll('.socials__path');
 
-svgIcons.forEach(svg => svg.addEventListener('mouseleave', removeFill));
-svgIcons.forEach(svg => svg.addEventListener('mouseover', fill));
+  function fill() {
+    const child = this.querySelector('.socials__path');
+    child.classList.add('socials__path--filled');
+  }
 
-//slider
-const slides = document.querySelectorAll('.slider__slide');
-const nextSlideButton = document.querySelector('.slider__button-right');
-const prevSlideButton = document.querySelector('.slider__button-left');
+  function removeFill() {
+    const child = this.querySelector('.socials__path');
+    child.classList.remove('socials__path--filled');
+  }
 
-let current = 0;
+  svgIcons.forEach(svg => svg.addEventListener('mouseleave', removeFill));
+  svgIcons.forEach(svg => svg.addEventListener('mouseover', fill));
 
-function goToSlide(n) {
-  slides[current].classList.remove('slider__slide--active');
-  current = (n + slides.length)%slides.length;
-  slides[current].classList.add('slider__slide--active');
-}
 
-function playSlide() {
-  goToSlide(current + 1);
-}
+  // Slider implementation
+  const slides = document.querySelectorAll('.slider__slide');
+  const nextSlideButton = document.querySelector('.slider__button-right');
+  const prevSlideButton = document.querySelector('.slider__button-left');
 
-function nextSlide() {
-  clearInterval(slideTimer);
-  goToSlide(current + 1);
-}
+  let current = 0;
 
-function prevSlide() {
-  clearInterval(slideTimer);
-  goToSlide(current - 1);
-}
+  function goToSlide(n) {
+    slides[current].classList.remove('slider__slide--active');
+    current = (n + slides.length)%slides.length;
+    slides[current].classList.add('slider__slide--active');
+  }
 
-let slideTimer = setInterval(playSlide, 3500);
-nextSlideButton.addEventListener('click', nextSlide);
-prevSlideButton.addEventListener('click', prevSlide);
+  function playSlide() {
+    goToSlide(current + 1);
+  }
 
-// smooth scrolling
-const root = $('html, body');
-const navLowHeight = 16 * 4; // 16px(root) * 4rem(nav--low);
+  function nextSlide() {
+    clearInterval(slideTimer);
+    goToSlide(current + 1);
+  }
 
-const mediaQuery = window.matchMedia('(max-width: 47.94rem)');
+  function prevSlide() {
+    clearInterval(slideTimer);
+    goToSlide(current - 1);
+  }
 
-// root px font-size * header rem font-size
-const headerLowHeight = mediaQuery.matches? 16 * 3 :  16 *  4
+  let slideTimer = setInterval(playSlide, 3500);
+  nextSlideButton.addEventListener('click', nextSlide);
+  prevSlideButton.addEventListener('click', prevSlide);
 
-$('a').click(function(e) {
 
-  $('.nav').removeClass('nav--open');
+  // Smooth scrolling
+  const root = $('html, body');
+  const navLowHeight = 16 * 4; // 16px(root) * 4rem(nav--low);
 
-  e.preventDefault();
-  const href = $.attr(this, 'href');
-  root.animate({
-    scrollTop: $(href).offset().top + 2 - headerLowHeight
-  }, 500, function() {
-    location.hash = href;
+  const mediaQuery = window.matchMedia('(max-width: 47.94rem)');
+  const headerLowHeight = mediaQuery.matches? 16 * 3 :  16 *  4 // root px font-size * header rem font-size
+
+  $('a').click(function(e) {
+
+    $('.nav').removeClass('nav--open');
+
+    e.preventDefault();
+    const href = $.attr(this, 'href');
+    root.animate({
+      scrollTop: $(href).offset().top + 2 - headerLowHeight
+    }, 500, function() {
+      location.hash = href;
+    });
+    return false;
   });
-  return false;
-});
 
-//scrolling nav
-const navLinks = document.querySelectorAll('.nav__link');
-const headerContainer = document.querySelector('.header__container');
-const test = document.querySelectorAll('.hidden');
-console.log(test);
 
-function changeNavItemActivation() {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  let scrollBottom = scrollTop + window.innerHeight;
+  // Active nav items when scrolling to specified section
+  const navLinks = document.querySelectorAll('.nav__link');
+  const headerContainer = document.querySelector('.header__container');
 
-  if (headerContainer.offsetHeight / 3 < scrollTop) {
-    headerContainer.classList.add('header--low');
-  }
-  else {
-    headerContainer.classList.remove('header--low');
-  }
+  function changeNavItemActivation() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollBottom = scrollTop + window.innerHeight;
 
-  navLinks.forEach(function(link) {
-    let currentLink = link;
-    let refId = currentLink.getAttribute('href').slice(1);
-    let refElement = document.getElementById(refId);
-
-    if (refElement.offsetTop <= scrollTop + navLowHeight && refElement.offsetTop + refElement.offsetHeight > scrollTop + navLowHeight) {
-      currentLink.classList.add('nav__link--active');
+    if (headerContainer.offsetHeight / 3 < scrollTop) {
+      headerContainer.classList.add('header--low');
     }
     else {
-      currentLink.classList.remove('nav__link--active');
+      headerContainer.classList.remove('header--low');
     }
-  });
 
-  test.forEach(function(elem) {
-    console.log(elem.offsetTop, elem.offsetHeight, scrollBottom);
-    if (elem.offsetTop + elem.offsetHeight / 2  <= scrollBottom) {
-      elem.classList.remove('hidden');
-    }
-  });
-}
+    navLinks.forEach(function(link) {
+      let currentLink = link;
+      let refId = currentLink.getAttribute('href').slice(1);
+      let refElement = document.getElementById(refId);
+    
+        if (refElement.offsetTop <= scrollTop + navLowHeight && refElement.offsetTop + refElement.offsetHeight > scrollTop + navLowHeight) {
+          currentLink.classList.add('nav__link--active');
+        }
+        else {
+          currentLink.classList.remove('nav__link--active');
+        }
+    });
+  }
+  document.addEventListener('scroll', changeNavItemActivation);
 
-document.addEventListener('scroll', changeNavItemActivation);
+
+  // Show and animate element when scroll reaches a half of the element's height 
+  const animatedElements = document.querySelectorAll('.hidden');
+
+  function animateAfterReach() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let scrollBottom = scrollTop + window.innerHeight;
+
+    animatedElements.forEach(function(elem) {
+      if (elem.offsetTop + elem.offsetHeight / 2  <= scrollBottom) {
+        elem.classList.remove('hidden');
+      }
+    });
+  }
+  document.addEventListener('scroll', animateAfterReach);
+
+}());
